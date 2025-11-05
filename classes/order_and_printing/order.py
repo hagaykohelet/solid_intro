c856002d14a6eca5bd2a887c2e3c70ddfcbe211c
hagay kohelet <hagay2506@gmail.com>
2025-11-05 09:51:40 +0200

    order and printing

--- classes/order_and_printing/order.py
+++ classes/order_and_printing/order.py
@@ -1,2 +1,13 @@
 class Order:
-    pass
+    def __init__(self, items:list[str], total_price:float):
+        self.items = items
+        self.total_price = total_price
+
+class InvoicePrinter:
+    @staticmethod
+    def print_items(order:Order):
+        for item in order.items:
+            print(item,end=" ")
+        print(f"price: {order.total_price}")
+
+
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from classes.studenta_nd_grade.student import Student,GradeCalculator
-
+from classes.order_and_printing.order import Order,InvoicePrinter
 if __name__ == "__main__":
-    s = Student("hagay",[1,2,3,4,5,6,7,8,9,10])
-    g  = GradeCalculator
-    g.average_grade(s)
+    # s = Student("hagay",[1,2,3,4,5,6,7,8,9,10])
+    # g  = GradeCalculator
+    # g.average_grade(s)
+
+    o = Order(["a","b","s"],5.5)
+    i = InvoicePrinter()
+    i.print_items(o)
